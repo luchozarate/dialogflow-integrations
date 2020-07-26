7692366f30fa68614d69012e846d93703ea954b2
luchozarate <68802899+luchozarate@users.noreply.github.com>
2020-07-26 03:27:02 -0300

    Update server.js

--- twilio/server.js
+++ twilio/server.js
@@ -31,8 +31,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const projectId = 'Place your dialogflow projectId here';
 const phoneNumber = "Place your twilio phone number here";
-const accountSid = 'Place your accountSid here';
-const authToken = 'Place your authToken here';
+const accountSid = 'AC217ec43a8cca6bcf850cdaeb338320d9';
+const authToken = '2a88770b537c1858ddc15cbbb4ff06c6';
 
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
